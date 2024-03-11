24eabdf0da9b8f782c8af2889ab79362a51b5381
whywhy <wuhy1@shanghaitech.edu.cn>
2024-03-11 19:34:12 +0800

    fix: env vars

--- run_generation.sh
+++ run_generation.sh
@@ -1,13 +1,13 @@
 # improvement: huge
-export ALGPT_FLASH_ATTN=1
+export TINY_FLASH_ATTN=1
 # improvement: significant
-export ALGPT_FUSED_RMSNORM=1
+export TINY_FUSED_RMSNORM=1
 # improvement: none
-export ALGPT_FUSED_CROSSENTROPY=1
+export TINY_FUSED_CROSSENTROPY=1
 # # improvement: none
-# export ALGPT_FUSED_ROTARY=1
+# export TINY_FUSED_ROTARY=1
 # improvement: slightly
-export ALGPT_FUSED_SWIGLU=1
+export TINY_FUSED_SWIGLU=1
 
 python run_generation.py \
     --model_type llama \
